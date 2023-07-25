23a59614231756e80f86d34f7c6a6102edf25f68
Wafol <pavel.krauz1@gmail.com>
2023-07-25 12:00:00 +0200

    New CMake Project

---
+++ ChessAlgorithm/ChessBoard.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pavel on 25.07.2023.
+//
+
+#include "ChessBoard.h"
---
+++ ChessAlgorithm/ChessBoard.h
@@ -0,0 +1,18 @@
+//
+// Created by pavel on 25.07.2023.
+//
+
+#ifndef CHESSALGORITHM_CHESSBOARD_H
+#define CHESSALGORITHM_CHESSBOARD_H
+
+
+class ChessBoard {
+public:
+
+private:
+
+
+};
+
+
+#endif //CHESSALGORITHM_CHESSBOARD_H
